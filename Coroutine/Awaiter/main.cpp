a9fd8fba82eab297dc984118c8f150c00bfdb7b5
DenyShew <deny-shew@yandex.ru>
2021-08-26 22:56:49 +0100

    added example

--- Coroutine/Awaiter/main.cpp
+++ Coroutine/Awaiter/main.cpp
@@ -1,13 +1,19 @@
 #include "task_handler.hpp"
 
+#include <iostream>
+
 task<void> foo()
 {
+    std::cout << "start" << std::endl;
     co_await task<void>::classic_awaiter(task<void>::classic_awaiter::TYPE_AWAIT::ALWAYS);
+    std::cout << "end" << std::endl;
 }
 
 int main()
 {
     auto val = foo();
+    val.resume();
+    std::cout << "resuming" << std::endl;
     val.resume();
     return 0;
 }
