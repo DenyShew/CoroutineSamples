d9216c01f4f535c2a580e675f95b25d603155975
DenyShew <deny-shew@yandex.ru>
2021-09-02 23:18:18 +0100

    another one refactoring

--- Coroutine/Awaiter/build/cmake_install.cmake
+++ Coroutine/Awaiter/build/cmake_install.cmake
@@ -43,11 +43,18 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
+  # Include the install script for the subdirectory.
   include("/home/denys/repo/Joys/Coroutine/Awaiter/build/Waiter/cmake_install.cmake")
-  include("/home/denys/repo/Joys/Coroutine/Awaiter/build/Task/cmake_install.cmake")
-  include("/home/denys/repo/Joys/Coroutine/Awaiter/build/TaskHandler/cmake_install.cmake")
+endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/denys/repo/Joys/Coroutine/Awaiter/build/Task/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/denys/repo/Joys/Coroutine/Awaiter/build/TaskHandler/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
