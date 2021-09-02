5597e9a97decc6f8daa7b50073b76e99227c2b4b
DenyShew <deny-shew@yandex.ru>
2021-09-02 22:35:29 +0100

    removed *.cpp -> template classes

--- Coroutine/Awaiter/Waiter/Waiter.hpp
+++ Coroutine/Awaiter/Waiter/Waiter.hpp
@@ -1,5 +1,29 @@
 #ifndef __WAITER_H__
 #define __WAITER_H__
 
+template<typename T>
+class Waiter
+{
+public:
+    Waiter(bool isReady = false)
+    {
+        this->isReady = isReady;
+    }
+
+    void SetStatus(bool isReady = false)
+    {
+        this->isReady = isReady;
+    }
+
+    bool GetStatus()
+    {
+        return isReady;
+    }
+    
+protected:
+
+    bool isReady;
+    T value;
+};
 
 #endif // __WAITER_H__
