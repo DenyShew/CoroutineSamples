9d2c397cb82a52cbcbb51865cb94c67fb5b51a7f
DenyShew <deny-shew@yandex.ru>
2021-09-07 15:06:27 +0100

    waiter ready

--- Coroutine/Awaiter/src/Task/Task.hpp
+++ Coroutine/Awaiter/src/Task/Task.hpp
@@ -1,5 +1,8 @@
 #ifndef __TASK_H__
 #define __TASK_H__
 
+#include "../Waiter/Waiter.hpp"
+
+class task
 
 #endif // __TASK_H__
--- Coroutine/Awaiter/src/Waiter/Waiter.hpp
+++ Coroutine/Awaiter/src/Waiter/Waiter.hpp
@@ -1,24 +1,58 @@
 #ifndef __WAITER_H__
 #define __WAITER_H__
 
-template<typename T>
-class Waiter
+#include <coroutine>
+#include <vector>
+
+template<class S_VAL, class R_VAL, class CORO_TYPE>
+class waiter
 {
 public:
-    Waiter(bool isReady = false);
-    void SetStatus(const bool& isReady);
-    bool GetStatus()const;
-
-public:
-    void SetValue(T value)
+    waiter(std::vector<std::coroutine_handle<CORO_TYPE>>& coro_handler, bool is_ready = false)
+        : hanlder(coro_handler)
+        , is_ready(is_ready)
     {
-        this->value = value;
+
     }
-    
+
+    bool await_ready()
+    {
+        return is_ready;
+    }
+    S_VAL await_suspend(std::coroutine_handle<CORO_TYPE> coro)noexcept
+    {
+        handler.emplace_back(coro);
+        return suspend_value;
+    }
+    R_VAL await_resume()noexcept
+    {
+        return resume_value;
+    }
+
+    void set_suspend(const S_VAL& value)
+    {
+        suspend_value = value;
+    }
+    void set_resume(const R_VAL& value)
+    {
+        resume_value = value;
+    }
+    void set_status(const bool& is_ready)
+    {
+        this->is_ready = is_ready;
+    }
+    bool get_status()const
+    {
+        return is_ready;
+    }
+
 protected:
 
-    bool isReady;
-    T value;
+    bool is_ready;
+    S_VAL suspend_value;
+    R_VAL resume_value;
+
+    std::vector<std::coroutine_handle<CORO_TYPE>>& handler;
 };
 
 #endif // __WAITER_H__
